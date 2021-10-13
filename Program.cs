62c25be9555c4b759290a15c71890b90821b4252
HubertMac <hubert.malina@gmail.com>
2021-10-13 12:09:05 +0200

    Inicjalizacja CS

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Console.WriteLine("halko dzien dobry.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ test.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Csharp
+{
+    class test
+    {
+        static void Main(string[] args){
+            Console.WriteLine("halko dzien dobry.");
+            Console.ReadKey();
+        }
+    }
+}
+
